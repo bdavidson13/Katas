36ecec079953e22c9d239b46994c612dffd63ea4
Brian <brian.j.davidson13@gmail.com>
2015-07-14 19:15:30 -0400

    first commit for kata http://www.codewars.com/kata/greed-is-good/csharp

---
+++ Kata/GreedIsGoodTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Katas.GreedUsGood;
+
+namespace Kata
+{
+    [TestClass]
+    public class GreedIsGoodTests
+    {
+        private int[] dice { get; set; }
+        private DiceGame game { get; set; }
+        [TestInitialize]
+        public void setup()
+        {
+            game = new DiceGame();
+        }
+
+        [TestMethod]
+        public void GetScore_PassedArrayWithASingle1_ScoreShouldBe100()
+        {
+            setDice(new int[] { 1, 2, 3, 4, 6 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 100, String.Format("score should be 100 but is {0}", score));
+        }
+        [TestMethod]
+        public void GetScore_PassedArrayWithThreeOnes_scoreShouldBe1000()
+        {
+            setDice(new int[] { 1, 3, 1, 4, 1 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 1000, String.Format("score should be 1000 but is {0}", score));
+        }
+        [TestMethod]
+        public void GetScore_PassedArrayWithALLOnes_scoreShouldBe1200()
+        {
+            setDice(new int[] { 1, 1, 1, 1, 1 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 1200, String.Format("score should be 1200 but is {0}", score));
+        }
+        [TestMethod]
+        public void GetScore_PassedArrayWithASingle5_ScoreShouldBe50()
+        {
+            setDice(new int[] { 5, 2, 3, 4, 6 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 50, String.Format("score should be 50 but is {0}", score));
+        }
+        [TestMethod]
+        public void GetScore_PassedArrayWithThree5s_scoreShouldBe500()
+        {
+            setDice(new int[] { 5, 3, 5, 4, 5 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 500, String.Format("score should be 500 but is {0}", score));
+        }
+        [TestMethod]
+        public void GetScore_PassedArrayWithALLFives_scoreShouldBe600()
+        {
+            setDice(new int[] { 5, 5, 5, 5, 5 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 600, String.Format("score should be 600 but is {0}", score));
+        }
+        [TestMethod]
+        public void GetScore_PassedArrayWithSingleOneAndSingleFive_scoreShouldBe150()
+        {
+            setDice(new int[] { 5, 1, 3, 6, 4 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 150, String.Format("score should be 150 but is {0}", score));
+        }
+        [TestMethod]
+        public void GetScore_PassedArrayWithThreeTwosAndNoFivesOrOnes_scoreShouldBe200()
+        {
+            setDice(new int[] { 2, 3, 2, 4, 2 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 200, String.Format("score should be 200 but is {0}", score));
+        }
+        [TestMethod]
+        public void GetScore_PassedArrayWithThreeThreesAndNoFivesOrOnes_scoreShouldBe300()
+        {
+            setDice(new int[] { 2, 3, 3, 4, 3 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 300, String.Format("score should be 300 but is {0}", score));
+        }
+        [TestMethod]
+        public void GetScore_PassedArrayWithThreeFoursAndNoFivesOrOnes_scoreShouldBe400()
+        {
+            setDice(new int[] { 2, 4, 3, 4, 4 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 400, String.Format("score should be 400 but is {0}", score));
+        }
+        [TestMethod]
+        public void GetScore_PassedArrayWithThreeSixsAndNoFivesOrOnes_scoreShouldBe600()
+        {
+            setDice(new int[] { 6, 4, 6, 4, 6 });
+            int score = game.GetScore(dice);
+            Assert.AreEqual(score, 600, String.Format("score should be 600 but is {0}", score));
+        }
+        private void setDice(int[] p_dice ) 
+        {
+            dice = p_dice;
+        }
+
+    }
+}
---
+++ Katas/GreedIsGood/DiceGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Katas.GreedUsGood
+{
+    public struct DiceGame
+    {
+        public int GetScore(int[] dice)
+        {
+            int score = 0;
+            foreach (var die in dice)
+            {
+                if (die == 1) score += 100;
+                if (die == 5) score += 50;
+            }
+            if (containsThree(dice, 1)) score += 700;
+            if (containsThree(dice, 2)) score += 200;
+            if (containsThree(dice, 3)) score += 300;
+            if (containsThree(dice, 4)) score += 400;
+            if (containsThree(dice, 5)) score += 350;
+            if (containsThree(dice, 6)) score += 600;
+            return score;
+        }
+
+        private static bool containsThree(int[] dice,int num)
+        {
+            return dice.Count(x => x == num) >= 3;
+        }
+    }
+}
