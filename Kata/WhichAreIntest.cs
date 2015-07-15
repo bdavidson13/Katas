7e07701cc41af65dfbc6e972dca20987e8887ad7
Brian <brian.j.davidson13@gmail.com>
2015-07-14 21:41:05 -0400

    Starting http://www.codewars.com/kata/which-are-in/train/csharp class made, first test written. Test fails

---
+++ Kata/WhichAreIntest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Katas;
+
+namespace Kata
+{
+ /*
+Given two arrays of strings a1 and a2 return a sorted array in lexicographical order and without duplicates of the 
+strings of a1 which are substrings of strings of a2.
+
+Example: a1 = ["arp", "live", "strong"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns ["arp", "live", "strong"]
+
+a1 = ["tarp", "mice", "bull"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns []
+
+Note: Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+  */ 
+    [TestClass]
+    public class WhichAreInTest
+    {
+        private WhichAreIn wai { get; set; } 
+
+        [TestMethod]
+        public void GetLexOrderedArray_GivenOneMatch_ReturnsMatch()
+        {
+            wai = new WhichAreIn();
+            string [] result = wai.GetLexOrderedArray(new String[]{"arp", "mice", "bull"},new String[]{"lively", "alive", "harp", "sharp", "armstrong"});
+        }
+    }
+}
---
+++ Katas/WhichAreIn/WhichAreIn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Katas
+{
+    public class WhichAreIn
+    {
+        public string[] GetLexOrderedArray(string[] p1, string[] p2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
