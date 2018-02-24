8962db8154933f70fa733f8b960b9a4fd493df92
Brian Davidson <brian.j.davidson13@gmail.com>
2018-02-24 12:29:44 -0500

    Did shortestWord Kata

---
+++ fsharp/ShortestWord/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let testString = "This is a test string"
+    testString.Split ' ' |> Array.sortBy (fun x -> x.Length)
+    |> fun arr -> Console.WriteLine(arr.[0].Length)
+    Console.ReadKey()    
+    0 // return an integer exit code
